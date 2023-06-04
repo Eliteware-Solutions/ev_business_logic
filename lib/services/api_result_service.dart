8ddf9435b17c1005dc8755f0d174885ee1be076a
Rahul Mishra <rahul.mishra605@gmail.com>
2023-06-04 16:21:45 +0530

    changing name of result constructor

--- lib/services/api_result_service.dart
+++ lib/services/api_result_service.dart
@@ -1,21 +1,21 @@
 abstract class Result<T> {
-  const Result.private();
+  const Result.empty();
 }
 
 class SuccessResult<T> extends Result<T> {
   final T? data;
   final String? message;
-  const SuccessResult({this.data, this.message}) : super.private();
+  const SuccessResult({this.data, this.message}) : super.empty();
 }
 
 class FailureResult<T> extends Result<T> {
   final dynamic error;
   final T? data;
-  const FailureResult({required this.error, this.data}) : super.private();
+  const FailureResult({required this.error, this.data}) : super.empty();
 }
 
 abstract class RepoResult<T> extends Result<T> {
-  const RepoResult._() : super.private();
+  const RepoResult._() : super.empty();
   const factory RepoResult.success({T data, String? message}) = RepoSuccess<T>;
   const factory RepoResult.failure({required dynamic error, T? data}) =
       RepoFailure<T>;
