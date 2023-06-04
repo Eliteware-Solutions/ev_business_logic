09143f57fb59c78c335281c6f6894d5f38299e49
Rahul Mishra <rahul.mishra605@gmail.com>
2023-06-04 16:20:02 +0530

    add user model in user state

--- lib/features/login/bloc/login_state.dart
+++ lib/features/login/bloc/login_state.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:ev_business_logic/features/login/model/login_response_model.dart';
 import 'package:formz/formz.dart';
 
 class LoginState extends Equatable {
@@ -6,12 +7,14 @@ class LoginState extends Equatable {
   final String? email;
   final String? password;
   final String? error;
+  final LoginData? userModel;
 
   const LoginState({
     this.registerStatus,
     this.email,
     this.password,
     this.error,
+    this.userModel,
   });
 
   LoginState copyWith({
@@ -19,15 +22,23 @@ class LoginState extends Equatable {
     String? email,
     String? password,
     String? error,
+    LoginData? userModel,
   }) {
     return LoginState(
       registerStatus: registerStatus ?? FormzStatus.pure,
       email: email ?? this.email,
       password: password ?? this.password,
       error: error ?? this.error,
+      userModel: userModel ?? this.userModel,
     );
   }
 
   @override
-  List<Object?> get props => [registerStatus, email, password, error];
+  List<Object?> get props => [
+        registerStatus,
+        email,
+        password,
+        error,
+        userModel,
+      ];
 }
--- lib/services/api_result_service.dart
+++ lib/services/api_result_service.dart
@@ -1,21 +1,21 @@
 abstract class Result<T> {
-  const Result._();
+  const Result.private();
 }
 
 class SuccessResult<T> extends Result<T> {
   final T? data;
   final String? message;
-  const SuccessResult({this.data, this.message}) : super._();
+  const SuccessResult({this.data, this.message}) : super.private();
 }
 
 class FailureResult<T> extends Result<T> {
   final dynamic error;
   final T? data;
-  const FailureResult({required this.error, this.data}) : super._();
+  const FailureResult({required this.error, this.data}) : super.private();
 }
 
 abstract class RepoResult<T> extends Result<T> {
-  const RepoResult._() : super._();
+  const RepoResult._() : super.private();
   const factory RepoResult.success({T data, String? message}) = RepoSuccess<T>;
   const factory RepoResult.failure({required dynamic error, T? data}) =
       RepoFailure<T>;
