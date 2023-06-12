0bc6b3dec1bc70ea593da84f4037106be421906c
Rahul Mishra <rahul.mishra605@gmail.com>
2023-06-12 11:36:34 +0530

    change end point

--- lib/resources/apis_end_points.dart
+++ lib/resources/apis_end_points.dart
@@ -1,6 +1,7 @@
 class ApisEndPoints {
   static const String baseUrl =
-      'https://9xnitpcah2.execute-api.ap-south-1.amazonaws.com/Prod/';
+      // 'https://9xnitpcah2.execute-api.ap-south-1.amazonaws.com/Prod/';
+      'https://0opxpo6j22.execute-api.ap-south-1.amazonaws.com/Prod/';
   static const String login = 'customer/login';
   static const String signUp = 'customer/register';
   static const String start = '/booking/start';
