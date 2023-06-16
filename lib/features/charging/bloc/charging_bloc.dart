699f49cca14388c1539f9621c3877f69085c0610
Shah Jash <83567342+shahjash1212@users.noreply.github.com>
2023-06-16 18:29:17 +0530

    removed delayed from the api start and stop

--- lib/features/charging/bloc/charging_bloc.dart
+++ lib/features/charging/bloc/charging_bloc.dart
@@ -2,7 +2,9 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:ev_business_logic/features/charging/bloc/charging_state.dart';
 import 'package:ev_business_logic/features/charging/model/charging_enum.dart';
+import 'package:ev_business_logic/features/charging/model/charging_response_model.dart';
 import 'package:ev_business_logic/features/charging/repo/charging_repo.dart';
+import 'package:ev_business_logic/services/api_result_service.dart';
 import 'package:formz/formz.dart';
 
 part 'charging_event.dart';
@@ -25,21 +27,21 @@ class ChargingBloc extends Bloc<ChargingEvent, ChargingState> {
         emit(state.copyWith(
             apiCallStatus: FormzStatus.submissionInProgress,
             chargingEnum: ChargingEnum.start));
-        await Future.delayed(const Duration(seconds: 3));
+        // await Future.delayed(const Duration(seconds: 3));
         emit(state.copyWith(
             apiCallStatus: FormzStatus.submissionSuccess,
             chargingEnum: ChargingEnum.start));
-        // RepoResult? response =
-        //     await _chargingRepository.startCharging(bookingId: event.bookingId);
-        // if (response is RepoSuccess) {
-        //   emit(state.copyWith(
-        //       apiCallStatus: FormzStatus.submissionSuccess,
-        //       chargingEnum: ChargingEnum.start));
-        // } else if (response is RepoFailure) {
-        //   emit(state.copyWith(
-        //       apiCallStatus: FormzStatus.submissionFailure,
-        //       error: response.error));
-        // }
+        RepoResult? response =
+            await _chargingRepository.startCharging(bookingId: event.bookingId);
+        if (response is RepoSuccess) {
+          emit(state.copyWith(
+              apiCallStatus: FormzStatus.submissionSuccess,
+              chargingEnum: ChargingEnum.start));
+        } else if (response is RepoFailure) {
+          emit(state.copyWith(
+              apiCallStatus: FormzStatus.submissionFailure,
+              error: response.error));
+        }
       } catch (e) {
         emit(state.copyWith(apiCallStatus: FormzStatus.submissionFailure));
       }
@@ -50,23 +52,23 @@ class ChargingBloc extends Bloc<ChargingEvent, ChargingState> {
         emit(state.copyWith(
             apiCallStatus: FormzStatus.submissionInProgress,
             chargingEnum: ChargingEnum.finish));
-        await Future.delayed(const Duration(seconds: 3));
+        // await Future.delayed(const Duration(seconds: 3));
         emit(state.copyWith(
             apiCallStatus: FormzStatus.submissionSuccess,
             chargingEnum: ChargingEnum.finish));
-        // RepoResult? response =
-        //     await _chargingRepository.stopCharging(bookingId: event.bookingId);
-        // if (response is RepoSuccess) {
-        //   ChargingResponseModel data = response.data;
-        //   emit(state.copyWith(
-        //       stopApiCallStatus: FormzStatus.submissionSuccess,
-        //       chargingEnum: ChargingEnum.finish,
-        //       message: data.message));
-        // } else if (response is RepoFailure) {
-        //   emit(state.copyWith(
-        //       stopApiCallStatus: FormzStatus.submissionFailure,
-        //       error: response.error));
-        // }
+        RepoResult? response =
+            await _chargingRepository.stopCharging(bookingId: event.bookingId);
+        if (response is RepoSuccess) {
+          ChargingResponseModel data = response.data;
+          emit(state.copyWith(
+              stopApiCallStatus: FormzStatus.submissionSuccess,
+              chargingEnum: ChargingEnum.finish,
+              message: data.message));
+        } else if (response is RepoFailure) {
+          emit(state.copyWith(
+              stopApiCallStatus: FormzStatus.submissionFailure,
+              error: response.error));
+        }
       } catch (e) {
         emit(state.copyWith(stopApiCallStatus: FormzStatus.submissionFailure));
       }
