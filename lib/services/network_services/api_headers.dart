3d5a9f36d15617419a5555c82f57bb368b0a05ae
Shah Jash <83567342+shahjash1212@users.noreply.github.com>
2023-06-16 15:39:15 +0530

    added api header access token

--- lib/services/network_services/api_headers.dart
+++ lib/services/network_services/api_headers.dart
@@ -1,14 +1,16 @@
+import 'package:ev_business_logic/services/storage_service.dart';
+
 class ApiHeaders {
   static Future<Map<String, dynamic>> getHeaders({String? tempToken}) async {
-    String token = '';
-    // String token = await AuthBloc().getToken();
+    String? token;
+    token = await AuthBloc().getToken();
     if (token == '') {
       token = tempToken ?? '';
     }
 
     Map<String, dynamic> headers = {
       'Content-Type': 'application/json',
-      'authorization': 'Bearer $token',
+      'aceess_token': token,
     };
 
     return headers;
