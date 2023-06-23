8dfd275c314ca5d0168e00eb9ab3ca21372b3ef6
Shah Jash <83567342+shahjash1212@users.noreply.github.com>
2023-06-23 23:32:21 +0530

    added auth bloc feature

--- lib/features/authentication/bloc/authentication_event.dart
+++ lib/features/authentication/bloc/authentication_event.dart
@@ -4,16 +4,19 @@ abstract class AuthenticationEvent extends Equatable {
   const AuthenticationEvent();
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
 }
 
-class AuthenticationStatusChanged extends AuthenticationEvent {
-  const AuthenticationStatusChanged(this.status);
-
+class AuthenticationStatusUpdate extends AuthenticationEvent {
   final AuthenticationStatus status;
+  final Function? redirect;
+  AuthenticationStatusUpdate({
+    required this.status,
+    required this.redirect,
+  });
 
   @override
-  List<Object> get props => [status];
+  List<Object?> get props => [status, redirect];
 }
 
 class AuthenticationLogoutRequested extends AuthenticationEvent {}
