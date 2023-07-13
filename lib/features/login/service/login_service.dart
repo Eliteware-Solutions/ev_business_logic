a4e95a398d1f1c33e45fffdd610a67debb0aef71
Shah Jash <shahjash1212@gmail.com>
2023-07-13 19:38:55 +0530

    new tenet id in login and register  API

--- lib/features/login/service/login_service.dart
+++ lib/features/login/service/login_service.dart
@@ -1,11 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:ev_business_logic/features/init/init.dart';
 import 'package:ev_business_logic/resources/apis_end_points.dart';
 import 'package:ev_business_logic/services/network_services/api_client.dart';
 import 'package:ev_business_logic/services/network_services/api_result.dart';
 
 class LoginService {
   Future<ApiResult> login(payload) async {
-    ApiResult apiResult =
-        await DioClient().post(ApisEndPoints.login, data: payload);
+    ApiResult apiResult = await DioClient().post(ApisEndPoints.login,
+        data: payload,
+        options:
+            Options(headers: {'tenant': EVSDKInit.getInstance().tenantId}));
     return apiResult;
   }
 }
