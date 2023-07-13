d0a750c9d106aed98d637e97478cc2c5d55ca846
rangholiyaprince <138556493+urviRamani0305@users.noreply.github.com>
2023-07-13 10:15:01 +0530

    fix issue for register screen

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/rahulmishra/Desktop/FlutterSDKs/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/rahulmishra/Desktop/Projects/ev_business_logic"
+export "FLUTTER_ROOT=/Users/ki/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ki/Desktop/Projects/ev_business_logic"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
--- lib/features/register/model/register_request_model.dart
+++ lib/features/register/model/register_request_model.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
+
 class RegisterRequestModel {
+  String? firstName;
+
+  String? lastName;
+  String? email;
+  String? password;
   RegisterRequestModel({
     this.firstName,
     this.lastName,
@@ -6,23 +13,72 @@ class RegisterRequestModel {
     this.password,
   });
 
-  String? firstName;
-  String? lastName;
-  String? email;
-  String? password;
+  RegisterRequestModel copyWith({
+    String? firstName,
+    String? lastName,
+    String? email,
+    String? password,
+  }) {
+    return RegisterRequestModel(
+      firstName: firstName ?? this.firstName,
+      lastName: lastName ?? this.lastName,
+      email: email ?? this.email,
+      password: password ?? this.password,
+    );
+  }
 
-  factory RegisterRequestModel.fromJson(Map<String, dynamic> json) =>
-      RegisterRequestModel(
-        firstName: json['first_name'],
-        lastName: json['last_name'],
-        email: json['email'],
-        password: json['password'],
-      );
+  Map<String, dynamic> toMap() {
+    final Map<String, dynamic> map = {};
+    if (firstName != null) {
+      map['firstName'] = firstName;
+    }
+    if (lastName != null) {
+      map['lastName'] = lastName;
+    }
+    if (email != null) {
+      map['email'] = email;
+    }
+    if (password != null) {
+      map['password'] = password;
+    }
+    return map;
+  }
 
-  Map<String, dynamic> toJson() => {
-        'first_name': firstName,
-        'last_name': lastName,
-        'email': email,
-        'password': password,
-      };
+  factory RegisterRequestModel.fromMap(Map<String, dynamic> map) {
+    return RegisterRequestModel(
+      firstName: map['firstName'] != null ? map['firstName'] as String : null,
+      lastName: map['lastName'] != null ? map['lastName'] as String : null,
+      email: map['email'] != null ? map['email'] as String : null,
+      password: map['password'] != null ? map['password'] as String : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory RegisterRequestModel.fromJson(String source) =>
+      RegisterRequestModel.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'RegisterRequestModel(firstName: $firstName, lastName: $lastName, email: $email, password: $password)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is RegisterRequestModel &&
+        other.firstName == firstName &&
+        other.lastName == lastName &&
+        other.email == email &&
+        other.password == password;
+  }
+
+  @override
+  int get hashCode {
+    return firstName.hashCode ^
+        lastName.hashCode ^
+        email.hashCode ^
+        password.hashCode;
+  }
 }
